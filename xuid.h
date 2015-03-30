c18d2f6d376620f9f0d2e30a333f2304ca4e8d2a
Karl Redgate <Karl.Redgate@gmail.com>
2015-03-30 08:25:57 -0400

    add uuid and guid util lib

---
+++ xuid.h
@@ -0,0 +1,53 @@
+
+/*
+ * Copyright (c) 2012 Karl N. Redgate
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/*
+ * UUID and GUID support structs
+ */
+
+#ifndef _UUID_H_
+#define _UUID_H_
+
+#include <stdint.h>
+
+typedef uint8_t uuid_t[16];
+
+/*
+ * Examination of actual VHDx files show that the byte ordering
+ * of GUIDs is odd.  Instead of just being 16 bytes in
+ * normal UUID order, it takes the form of the structure listed
+ * in appendix A of the 1.0 spec.
+ */
+
+typedef struct {
+    uint32_t data1;
+    uint16_t data2;
+    uint16_t data3;
+    uint8_t  data4[8];
+} guid_t;
+
+int parse_uuid( char *, uuid_t );
+
+#endif
+
+/* vim: set autoindent expandtab sw=4 syntax=c: */
