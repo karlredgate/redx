450272afa41bd23fd0bad444136cafaecc7d859d
Karl Redgate <Karl.Redgate@gmail.com>
2015-04-27 10:10:14 -0400

    reformat objs

--- Darwin.mk
+++ Darwin.mk
@@ -1,4 +1,8 @@
 
-PLATFORM_OBJS := DarwinThread.o DarwinKernelEvent.o DarwinInterface.o DarwinBridge.o syslog_logger.o
+PLATFORM_OBJS  = DarwinThread.o
+PLATFORM_OBJS += DarwinKernelEvent.o
+PLATFORM_OBJS += DarwinInterface.o
+PLATFORM_OBJS += DarwinBridge.o
+PLATFORM_OBJS += syslog_logger.o
 
 DarwinThread.o :: PlatformThread.h
