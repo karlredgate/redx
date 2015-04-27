27059a55272d3aa671c7de7c077de6831362ee1d
Karl Redgate <Karl.Redgate@gmail.com>
2015-04-27 10:10:45 -0400

    add net mon to build

--- Darwin.mk
+++ Darwin.mk
@@ -3,6 +3,7 @@ PLATFORM_OBJS  = DarwinThread.o
 PLATFORM_OBJS += DarwinKernelEvent.o
 PLATFORM_OBJS += DarwinInterface.o
 PLATFORM_OBJS += DarwinBridge.o
+PLATFORM_OBJS += DarwinNetworkMonitor.o
 PLATFORM_OBJS += syslog_logger.o
 
 DarwinThread.o :: PlatformThread.h
