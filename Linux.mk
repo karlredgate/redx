3040ff01a5e1b4e7f068509a6d6a86ba54cbf8b0
Karl Redgate <Karl.Redgate@gmail.com>
2015-04-26 22:52:06 -0400

    add linuxbridge

--- Linux.mk
+++ Linux.mk
@@ -4,9 +4,9 @@ CFLAGS += -std=c99
 # PLATFORM_OBJS := NetLink.o LinuxThread.o Bridge.o
 PLATFORM_OBJS  = LinuxThread.o
 PLATFORM_OBJS += Kernel.o
-PLATFORM_OBJS += Bridge.o
 PLATFORM_OBJS += syslog_logger.o
 PLATFORM_OBJS += LinuxInterface.o
+PLATFORM_OBJS += LinuxBridge.o
 PLATFORM_OBJS += LinuxKernelEvent.o
 PLATFORM_OBJS += LinuxNetworkMonitor.o
 PLATFORM_OBJS += TCL_NetLink.o
